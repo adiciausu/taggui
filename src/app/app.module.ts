20c709011171eca152636cb2043edd8373b03aac
Adi Ciausu <adrian.ciausu@cegeka.com>
2019-06-13 16:44:12 +0300

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CoreModule} from './core/core.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     // angular
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
 
     // core
     CoreModule,
--- src/app/modules/annotate/annotate.module.ts
+++ src/app/modules/annotate/annotate.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {AnnotateComponent} from './components/pages/annotate/annotate.component';
 import {AnnotateRoutingModule} from './annotate-routing.module';
-import {SharedModule} from '../../shared/shared.module';
 import {CanvasD3Module} from '../canvas-d3/canvas-d3.module';
+import {ClassService} from '../class/service/class.service';
+import {ListboxModule} from 'primeng/listbox';
 
 
 @NgModule({
@@ -10,10 +11,13 @@ import {CanvasD3Module} from '../canvas-d3/canvas-d3.module';
     AnnotateComponent
   ],
   imports: [
-    SharedModule,
     AnnotateRoutingModule,
 
-    CanvasD3Module
+    CanvasD3Module,
+    ListboxModule
+  ],
+  providers: [
+    ClassService
   ]
 })
 export class AnnotateModule {
