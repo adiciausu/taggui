cfa264d200b3e4346edfae2ae9d0c2133972e6ef
Adi Ciausu <adrian.ciausu@cegeka.com>
2019-06-13 14:00:22 +0300

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,8 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  sidebarActive: boolean;
 
-  onMenuButtonClick(event: Event) {
-    this.sidebarActive = !this.sidebarActive;
-
-    event.preventDefault();
-  }
 }
