7c2aef6884efe5ebf69742c64efc2ef913e3aa7d
Adi Ciausu <adrian.ciausu@cegeka.com>
2019-06-14 11:31:54 +0300

    fixes

--- src/app/modules/annotate/components/pages/annotate/annotate.component.ts
+++ src/app/modules/annotate/components/pages/annotate/annotate.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
 import {ClassService} from '../../../../class/service/class.service';
 import {Class} from '../../../../class/models/class.model';
+import {CanvasD3Component} from '../../../../canvas-d3/components/canvas-d3/canvas-d3.component';
 
 @Component({
   selector: 'app-annotate',
@@ -11,6 +12,8 @@ export class AnnotateComponent implements OnInit {
   classes: Class[];
   selectedClass: Class;
 
+  @ViewChild(CanvasD3Component, {static: false}) canvasd3Component: CanvasD3Component;
+
   constructor(private classService: ClassService) {
   }
 
@@ -19,5 +22,15 @@ export class AnnotateComponent implements OnInit {
       this.classes = items;
       this.selectedClass = items[0];
     });
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if ((event.key <= 0) || (event.key > 9) || !(this.classes[event.key - 1])) {
+      return;
+    }
+
+    this.selectedClass = this.classes[event.key - 1];
+    this.canvasd3Component.drawClassAtCurrentMouseCorrds(this.selectedClass);
   }
 }
--- src/app/modules/canvas-d3/components/canvas-d3/canvas-d3.component.ts
+++ src/app/modules/canvas-d3/components/canvas-d3/canvas-d3.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import * as d3 from 'd3/index';
+import {Class} from '../../../class/models/class.model';
 
 @Component({
   selector: 'app-canvas-d3',
@@ -7,8 +8,11 @@ import * as d3 from 'd3/index';
   styleUrls: ['./canvas-d3.component.css']
 })
 export class CanvasD3Component implements OnInit {
+  @Input() selectedClass: Class;
+
   svg;
   rectangleStrokeWidth = 5;
+  currentMouseCoords = [];
 
   constructor() {
   }
@@ -17,33 +21,86 @@ export class CanvasD3Component implements OnInit {
     this.initSVG(800, 468);
   }
 
-  initSVG(width: number, height: number) {
+  drawClassAtCurrentMouseCorrds(clazz: Class) {
+    return this.drawClass(this.currentMouseCoords[0], this.currentMouseCoords[1], 100, 100, clazz);
+  }
+
+
+  drawClass(x: number, y: number, width: number, height: number, clazz: Class) {
+    if (!clazz) {
+      throw new Error('No class selected');
+    }
+
+    // only rectangle implemented
+    const rectangleGroup = this.svg.append('g');
+
+    // add rectangle
+    rectangleGroup.append('rect')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('stroke', 'orange')
+    .attr('stroke-width', this.rectangleStrokeWidth)
+    .attr('fill', clazz.color)
+    .style('opacity', 0.4)
+    .attr('cursor', 'move');
+
+    // add rectangle resize hotspots
+    this.addResizeHotspot(rectangleGroup, 0, 0, 'NW');
+    this.addResizeHotspot(rectangleGroup, width, 0, 'NE');
+    this.addResizeHotspot(rectangleGroup, 0, height, 'SW');
+    this.addResizeHotspot(rectangleGroup, width, height, 'SE');
+
+    // add drag behaviour
+    rectangleGroup
+    .datum({x: x, y: y})
+    .attr('transform', (d) => {
+      return 'translate(' + [d.x, d.y] + ')';
+    })
+    .call(
+      d3.drag()
+      .on('drag', (d: { x: number, y: number }) => {
+        rectangleGroup.attr('transform', (datum) => {
+          datum.x += d3.event.dx;
+          datum.y += d3.event.dy;
+
+          return 'translate(' + [datum.x, datum.y] + ')';
+        });
+      })
+    );
+  }
+
+  private initSVG(width: number, height: number) {
     this.svg = d3.select('#canvas-d3')
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height);
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
 
     // add image
     this.svg.append('image')
-      .attr('x', 0)
-      .attr('y', 0)
-      .attr('xlink:href', 'https://dz5vhvq2e26ss.cloudfront.net/media/image/7595667854e9da321.01809399.jpg')
-      .on('click', () => {
-        const coords = d3.mouse(d3.event.target);
-        this.drawRectangle(coords[0], coords[1], 100, 100);
-      });
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('xlink:href', 'https://dz5vhvq2e26ss.cloudfront.net/media/image/7595667854e9da321.01809399.jpg')
+    .on('click', () => {
+      const coords = d3.mouse(d3.event.target);
+      this.drawClass(coords[0], coords[1], 100, 100, this.selectedClass);
+    });
 
     // add zoom
     const zoom = d3.zoom()
-      .scaleExtent([.5, 2])
-      .on('zoom', () => {
-        this.svg.attr('transform', d3.event.transform);
-      });
-
+    .scaleExtent([.5, 2])
+    .on('zoom', () => {
+      this.svg.attr('transform', d3.event.transform);
+    });
     this.svg.call(zoom);
+
+
+    // start mouse move listener
+    this.svg.on('mousemove', () => {
+      this.currentMouseCoords = d3.mouse(d3.event.target);
+    });
   }
 
-  addResizeHotspot(rectangleGroup, x: number, y: number, className: string) {
+  private addResizeHotspot(rectangleGroup, x: number, y: number, className: string) {
     const resize = d3.drag().on('drag', (d) => {
       const rect = rectangleGroup.select('rect');
 
@@ -51,9 +108,9 @@ export class CanvasD3Component implements OnInit {
         const newWidth = d3.event.x - rect.attr('x');
         rect.attr('width', newWidth);
         rectangleGroup.select('.SE')
-          .attr('cx', newWidth)
+        .attr('cx', newWidth);
         rectangleGroup.select('.NE')
-          .attr('cx', newWidth)
+        .attr('cx', newWidth);
       } else {
         // const newWidth = rect.attr('x') - d3.event.x;
         // rect.attr('width', newWidth);
@@ -70,60 +127,21 @@ export class CanvasD3Component implements OnInit {
 
         rect.attr('height', newHeight);
         rectangleGroup.select('.SE')
-          .attr('cy', newHeight);
+        .attr('cy', newHeight);
         rectangleGroup.select('.SW')
-          .attr('cy', newHeight)
+        .attr('cy', newHeight);
       } else {
         //
       }
     });
 
     rectangleGroup.append('circle')
-      .classed(className, true)
-      .attr('fill', 'white')
-      .attr('r', this.rectangleStrokeWidth > 1 ? this.rectangleStrokeWidth - 1 : 1)
-      .attr('cx', x)
-      .attr('cy', y)
-      .attr('cursor', className.toLowerCase() + '-resize')
-      .call(resize);
-  }
-
-
-  drawRectangle(x: number, y: number, width: number, height: number) {
-    const rectangleGroup = this.svg.append('g');
-
-    // add rectangle
-    rectangleGroup.append('rect')
-      .attr('width', width)
-      .attr('height', height)
-      .attr('stroke', 'orange')
-      .attr('stroke-width', this.rectangleStrokeWidth)
-      .attr('fill', '#2378ae')
-      .style('opacity', 0.4)
-      .attr('cursor', 'move');
-
-    // add rectangle resize hotspots
-    this.addResizeHotspot(rectangleGroup, 0, 0, 'NW');
-    this.addResizeHotspot(rectangleGroup, width, 0, 'NE');
-    this.addResizeHotspot(rectangleGroup, 0, height, 'SW');
-    this.addResizeHotspot(rectangleGroup, width, height, 'SE');
-
-    // add drag behaviour
-    rectangleGroup
-      .datum({x: x, y: y})
-      .attr('transform',  (d) => {
-        return 'translate(' + [d.x, d.y] + ')';
-      })
-      .call(
-        d3.drag()
-          .on('drag', (d: {x: number, y: number}) => {
-            rectangleGroup.attr('transform', (datum) => {
-              datum.x += d3.event.dx;
-              datum.y += d3.event.dy;
-
-              return 'translate(' + [datum.x, datum.y] + ')';
-            });
-          })
-      );
+    .classed(className, true)
+    .attr('fill', 'white')
+    .attr('r', this.rectangleStrokeWidth > 1 ? this.rectangleStrokeWidth - 1 : 1)
+    .attr('cx', x)
+    .attr('cy', y)
+    .attr('cursor', className.toLowerCase() + '-resize')
+    .call(resize);
   }
 }
