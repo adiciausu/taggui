270db45624180b890ff38ab94c353cfb2219271c
Adi Ciausu <adi@Adis-MacBook-Pro-2.local>
2019-07-01 15:36:51 +0300

    move log server

--- src/app/shared/components/menu-logged/menu-logged.component.ts
+++ src/app/shared/components/menu-logged/menu-logged.component.ts
@@ -38,6 +38,7 @@ export class MenuLoggedComponent implements OnInit {
 
   ngOnInit(): void {
     this.selectedProjectId$.subscribe((item) => {
+      console.log(item);
       this.selectedProjectId = item;
     });
     this.store.dispatch(new LoadProjectsAction());
